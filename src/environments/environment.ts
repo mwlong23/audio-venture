bed5c51e32f736256cdb7ee26158cc44cc0d62e8
Mitch <meechllada@gmail.com>
2018-01-21 14:15:22 -0800

    add template to course component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false, 
   firebase: {
-    // apiKey: "["AIzaSyBHDd0D4vaROivMKFhkf7iPrfmyvF-GJP0"]", 
-    // authDomain: "["audio-venture.firebaseapp.com"]",
-    // databaseURL: "["https://audio-venture.firebaseio.com" ]",
-    // projectId: "["audio-venture"]",
-    // storageBucket: "[ ]",
-    // messagingSenderId: "["770193746351"]"
+    apiKey: "AIzaSyBHDd0D4vaROivMKFhkf7iPrfmyvF-GJP0", 
+    authDomain: "audio-venture.firebaseapp.com",
+    databaseURL: "https://audio-venture.firebaseio.com",
+    projectId: "audio-venture",
+    storageBucket: "audio-venture.appspot.com",
+    messagingSenderId: "770193746351"
   }
 };
